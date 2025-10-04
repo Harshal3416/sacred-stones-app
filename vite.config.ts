55b6b631f3b9ba4d9bf6a5f04b2cd4ef1e155bda
harshal3416 <harshalbelamkar3416@gmail.com>
2025-10-04 13:00:47 +0530

    gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: "/sacred-stones-app",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
